fef27e534fe571fd7f7215628241ed0e8e1e6d76
kh595 <ohkyuho@gmail.com>
2023-11-22 22:38:05 +0900

    deploy practice

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
     reactStrictMode: true,
-    output:'export',
+    // output:'export',    
     basePath : "/kh595.github.io",
     trailingSlash: true,
 }
