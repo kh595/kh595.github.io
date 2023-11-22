9b54643f3140fabc09659fa11f924078d91fd3ac
kh595 <ohkyuho@gmail.com>
2023-11-22 22:11:42 +0900

    folder change

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    output:'export',
+    basePath : "/kh595.github.io",
+    trailingSlash: true,
+}
+
+module.exports = nextConfig
