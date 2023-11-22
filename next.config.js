7e4bcb4b452d80cfa53a1ea70828b7929e4e990a
kh595 <ohkyuho@gmail.com>
2023-11-22 23:35:37 +0900

    deploy 2023-11-22 final

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    reactStrictMode: true,
-    // output:'export',    
-    basePath : "/kh595.github.io",
+    reactStrictMode: true,    
+    basePath: process.env.NODE_ENV === 'production' ? '/kh595.github.io' : '',
     trailingSlash: true,
 }
 
