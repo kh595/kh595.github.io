506ac99f614e18c61ca9ea7688a869acd4e9af06
kh595 <kh595@github.com>
2024-01-27 15:50:44 +0900

    add data editor

---
+++ app/network/edit/page.js
@@ -0,0 +1,30 @@
+'use client'
+import { HotTable } from '@handsontable/react';
+import { registerAllModules } from 'handsontable/registry';
+import 'handsontable/dist/handsontable.full.min.css';
+import data from '../books.json';
+
+registerAllModules();
+
+const ExampleComponent = () => {
+  return (
+      <HotTable
+        // set `HotTable`'s props here
+        // data={[
+        //   ['', 'Tesla', 'Volvo', 'Toyota', 'Ford'],
+        //   ['2019', 10, 11, 12, 13],
+        //   ['2020', 20, 11, 14, 13],
+        //   ['2021', 30, 15, 12, 13]
+        // ]}
+        data = {data.nodes}
+        rowHeaders={true}
+        colHeaders={true}
+        height="auto"
+        licenseKey="non-commercial-and-evaluation" // for non-commercial use only
+      />
+  );
+};
+
+export default function Home() {
+    return <ExampleComponent/>
+}
