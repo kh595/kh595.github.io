b2ecc42a2343cf56a47f8cf0f2b075d57d4898dd
kh595 <kh595@github.com>
2026-01-16 12:23:06 +0900

    Fix Handsontable cell type error: Register DropdownCellType

--- app/network/edit/links/page.js
+++ app/network/edit/links/page.js
@@ -14,6 +14,7 @@ import {
 
 import {
     CheckboxCellType,
+    DropdownCellType,
     NumericCellType,
     registerCellType,
 } from 'handsontable/cellTypes';
@@ -21,6 +22,7 @@ import { HotTable } from '@handsontable/react';
 import 'handsontable/dist/handsontable.full.css';
 
 registerCellType(CheckboxCellType);
+registerCellType(DropdownCellType);
 registerCellType(NumericCellType);
 
 registerPlugin(AutoColumnSize);
